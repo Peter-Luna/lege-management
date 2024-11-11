abf5ce27594419c5eb27219d107600522872923e
lph1545 <1545985939@qq.com>
2024-11-11 21:22:43 +0800

    first submission

---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+//争取的样式引入顺序
+// 样式初始化一般放在最前面
+import 'reset-css'
+//UI 框架的样式
+
+// 全局样式
+import './assets/styles/global.scss'
+//组件的样式
+import App from './App.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
